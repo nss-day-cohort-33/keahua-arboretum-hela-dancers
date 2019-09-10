80af4391c2b1c05267cf7fc12373826bf708e64c
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-09-10 13:22:04 -0500

    added different isIstances into release animal to match cultivate plants

--- arboretum.py
+++ arboretum.py
@@ -1,19 +1,22 @@
-from environments import River
-from environments import Swamp
+# TODO: Delete code after merges are completed
+# from environments import River
+# from environments import Swamp
 
-river1 = River("River", 12, 6)
-river2 = River("River", 12, 6)
-swamp1 = Swamp("Swamp", 8, 3)
-swamp2 = Swamp("Swamp", 8, 2)
+# river1 = River("River", 12, 6)
+# river2 = River("River", 12, 6)
+# swamp1 = Swamp("Swamp", 8, 3)
+# swamp2 = Swamp("Swamp", 8, 2)
 
 
 class Arboretum:
     def __init__(self, name, address):
         self.name = name
         self.address = address
-        self.rivers = [river1, river2]
+        # self.rivers = [river1, river2]
+        self.rivers = []
         self.grasslands = []
         self.coastlines = []
         self.mountains = []
         self.forests = []
-        self.swamps = [swamp1, swamp2]
+        # self.swamps = [swamp1, swamp2]
+        self.swamps = []
